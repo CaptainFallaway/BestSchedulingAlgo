b6f2be32c93e1d1b55f0ec17922218ea8b39e7c3
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-04-22 10:24:43 +0200

    fix: rename & fix of function names

--- graphing/syncer.go
+++ graphing/syncer.go
@@ -10,17 +10,18 @@ type Syncer struct {
 	wg sync.WaitGroup
 }
 
-func (s *Syncer) Start(delta int, c *chan TermPixel) {
+func (s *Syncer) start(delta int, c *chan TermPixel) {
 	s.wg.Add(delta)
-	go s.loop(c)
+	go s.waiter(c)
 }
 
+// When the render function is done, call this to decrement to sync all the components goroutines
 func (s *Syncer) Done() {
 	// Inline sync done call, removing overhead of a function call
 	s.wg.Add(-1)
 }
 
-func (s *Syncer) loop(c *chan TermPixel) {
+func (s *Syncer) waiter(c *chan TermPixel) {
 	s.wg.Wait()
 	close(*c)
 }
